95eb93825e56f115ed8450b7aeb13c1bfb8324f7
tfleish <tfleish@mit.edu>
2011-03-29 04:43:17 -0400

    hard-coded task list... can't do anything with it...

--- js/bucketList.js
+++ js/bucketList.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
 	$("#sticky1").draggable( {containment: "#bulletin"} );	
 	$("#sticky2").draggable( {containment: "#bulletin"} );
+	
+	$("#accordion").accordion();
   
 
 });
