cc97926d73a62bb59e85b4e3b7e2254bbd55b130
tfleish <tfleish@mit.edu>
2011-04-07 02:57:17 -0400

    test push to dotcloud

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var sys = require('sys');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var rs = fs.createReadStream('index.html');
+  sys.pump(rs, response);
+
+}).listen(9316);
